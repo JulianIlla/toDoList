f64812bc4c7a3a8f2bfef09ce9b76753e282d71d
JulianIlla <jilla23211@columbushs.com>
2021-02-11 08:31:44 -0500

    finished last video

--- toDoList-1/toDoList/app.js
+++ toDoList-1/toDoList/app.js
@@ -1,6 +1,6 @@
 const addForm = document.querySelector('.add');
+const search = document.querySelector('.search input');
 const list = document.querySelector('.todos');
-
 
 const generateTemplate = todo => {
   const html = `
@@ -12,6 +12,17 @@ const generateTemplate = todo => {
   list.innerHTML += html;
 };
 
+const filterTodos = term => {
+
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
 addForm.addEventListener('submit', e => {
   
   e.preventDefault();
@@ -21,4 +32,17 @@ addForm.addEventListener('submit', e => {
     generateTemplate(todo);
     addForm.reset();
   }
+});
+
+list.addEventListener('click', e => {
+
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+});
+
+search.addEventListener('keyup', () => {
+
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
